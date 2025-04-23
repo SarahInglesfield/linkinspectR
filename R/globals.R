66adaa9cc44b672eaee4d3144d974b32323bab6f
Sarah Inglesfield <inglesfs@babraham.ac.uk>
2025-04-23 21:18:47 +0100

    added function scripts

---
+++ R/globals.R
@@ -0,0 +1,5 @@
+utils::globalVariables(c("nCount_RNA","gene_data","count_type","gene","peak",
+                         "label_name","score","link","read_in_peak","gene_counts",
+                         "gene_expressed","expressed","total_observations",
+                         "percent_expressed","lower_conf_int","upper_conf_int",
+                         "pvalue","zscore","peak_counts","cell_barcode"))
