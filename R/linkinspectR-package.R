35843a7c330dc2c3dbbcb2ba5908dc8636fc1f4e
Sarah Inglesfield <inglesfs@babraham.ac.uk>
2025-05-11 17:47:51 +0100

    added package documentation

---
+++ R/linkinspectR-package.R
@@ -0,0 +1,15 @@
+#' linkinspectR: A package for reviewing peak-to-gene links in multiomic single-cell datasets
+#'
+#' The linkinspectR package provides three main functions
+#'
+#' @section linkinspectR functions:
+#' get_link_data
+#'
+#' by_peak_expression
+#'
+#' by_peak_proportion
+#'
+#' @aliases linkinspectR
+"_PACKAGE"
+#' @name linkinspectR
+NULL
